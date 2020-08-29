5d325cc962d759e84444a42faf378e25eb90201c
SerbanConstantin <serban.g.constantin@gmail.com>
2020-08-29 10:12:25 +0300

    update service

---
+++ src/main/java/com/rosu/onlinestore/security/DatabaseUserDetailsService.java
@@ -0,0 +1,59 @@
+package com.rosu.onlinestore.security;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import onlinecatalog.onlinecatalog.model.PendingUser;
+import onlinecatalog.onlinecatalog.model.User;
+import onlinecatalog.onlinecatalog.repository.PendingUserRepository;
+import onlinecatalog.onlinecatalog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@Getter
+@Slf4j
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+    
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private PendingUserRepository pendingUserRepository;
+
+    @Autowired
+    public DatabaseUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Username " + username);
+        Optional<PendingUser> optional = pendingUserRepository.findByUsername(username);
+
+        if (optional.isPresent()) {
+            log.info(optional.get().getActivationCode());
+            throw new UsernameNotFoundException(username);
+        }
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new CustomUserDetails(user);
+    }
+    
+//    public UserDetails loadUserByJwtToken(String jwtToken){
+//
+//        if (jwtProvider.isTokenValid(jwtToken){
+//           return Optional.of(
+//                   withUsername(jwtProvider.getUsername(jwtToken))
+//                   .password("")//
+//                   .
+//           )
+//        }
+//    }
+}
---
+++ src/main/java/com/rosu/onlinestore/security/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.rosu.onlinestore.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private String secretKey;
+    private long validityInMilliseconds;
+
+    @Autowired
+    public JwtProvider(@Value("${security.jwt.token.secret-key}") String secretKey,
+                       @Value("${security.jwt.token.expiration}") long validityInMilliseconds) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+    public void createToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+
+        Date now = new Date();
+        Date expiresAt = new Date(now.getTime() + validityInMilliseconds);
+
+        Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiresAt)
+                .signWith(SignatureAlgorithm.ES256, secretKey)
+                .compact();
+
+    }
+
+    public boolean isTokenValid(String jwtToken) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return true;
+
+        } catch (JwtException ex) {
+
+        }
+        return false;
+    }
+
+    public String getUsername(String jwtToken) {
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(jwtToken).getBody().getSubject();
+    }
+}
